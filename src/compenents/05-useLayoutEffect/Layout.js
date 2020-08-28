5857b4a7ce6c487ad382fe6f9f33a1dad845f915
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-08-28 15:44:18 -0400

    implementacion de set Box Size

--- src/compenents/05-useLayoutEffect/Layout.js
+++ src/compenents/05-useLayoutEffect/Layout.js
@@ -2,6 +2,7 @@ import React, { useRef, useLayoutEffect } from "react";
 import "./layout.css";
 import { useFectch } from "../../hooks/useFectch";
 import { useCounter } from "../../hooks/useCounter";
+import { useState } from "react";
 
 
 
@@ -13,12 +14,14 @@ export const Layout = () => {
     `https://www.breakingbadapi.com/api/quotes/${state}`
   );
 
+    const [ boxSize, setBoxSize] = useState({})
+
   const { quote } = !!data && data[0];
   const pTag = useRef();
 
   useLayoutEffect(() => {
 
-     console.log( pTag.current.getBoundingClientRect() );
+     setBoxSize( pTag.current.getBoundingClientRect() );
       
   }, [quote])
 
@@ -29,8 +32,11 @@ export const Layout = () => {
       
         <blockquote className="blockquote text-right">
           <p className="mb-0" ref= {pTag}>{quote}</p>
-          
         </blockquote>
+
+        <pre>
+          {JSON.stringify( boxSize, null, 3 )}
+        </pre>
     
 
       <button className="btn btn-primary" onClick={increment}>
